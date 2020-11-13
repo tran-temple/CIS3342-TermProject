b09f5c29ac1e443babad14d95d9aa505cc0d451c
Rish Iyer <34191223+rishvaiyer@users.noreply.github.com>
2020-11-13 11:27:28 -0500

    removed UC nav

--- CIS3342-TermProject/MasterPage.Master.designer.cs
+++ CIS3342-TermProject/MasterPage.Master.designer.cs
@@ -40,12 +40,21 @@ namespace CIS3342_TermProject {
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
         
         /// <summary>
-        /// AdminNavUC1 control.
+        /// panelNavAdmin control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::CIS3342_TermProject.AdminNavUC AdminNavUC1;
+        protected global::System.Web.UI.WebControls.Panel panelNavAdmin;
+        
+        /// <summary>
+        /// lblLoggedInAs control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label lblLoggedInAs;
     }
 }
