12f7324f5bac9037e741e80549451deae89c5d4d
tran-temple <tuk85873@temple.edu>
2020-11-12 22:53:50 -0500

    remove credential info

--- EcommerceLibrary/Connection.cs
+++ EcommerceLibrary/Connection.cs
@@ -9,10 +9,7 @@ using System.Data.SqlClient;
 namespace EcommerceLibrary
 {
     public class DBConnect
-    {
-        // Main Connection String - used for the published web application and project submissions.
-        //String SqlConnectString = "server=cis-mssql1.temple.edu;Database=fa20_3342_tuk85873;User id=tuk85873;Password=iV3EiP9Uhe";
-
+    {        
         // Home Connection String - used for working from home using SSH Tunneling.
         String SqlConnectString = "server=127.0.0.1,5555;Database=fa20_3342_tug60798;User id=tug60798;Password=kaemez9e";
 
