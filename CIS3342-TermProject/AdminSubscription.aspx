5d4c161c8b4b4703523b82abe014346961581063
Rish Iyer <34191223+rishvaiyer@users.noreply.github.com>
2020-11-14 12:57:16 -0500

    Admin Subscription page updated
    Admin edit subscription page added grid view, allowed for editing and added button to open modal to allow add new subscription

--- CIS3342-TermProject/AdminSubscription.aspx
+++ CIS3342-TermProject/AdminSubscription.aspx
@@ -2,4 +2,72 @@
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="BodyPlaceHolder" runat="server">
+
+    <asp:Label ID="lblError" runat="server" Text=""></asp:Label>
+                <asp:GridView ID="gvSubscriptions" runat="server" AutoGenerateColumns="False" BackColor="White" BorderColor="#CCCCCC" BorderStyle="None" BorderWidth="1px" CellPadding="4" ForeColor="Black" GridLines="Horizontal"  EnableViewState="False" OnRowEditing="gvSubscriptions_RowEditing" OnRowUpdating="gvSubscriptions_RowUpdating" OnSelectedIndexChanged="gvSubscriptions_SelectedIndexChanged" OnRowCancelingEdit="gvSubscriptions_RowCancelingEdit">
+        <Columns>
+                        <asp:BoundField DataField="SubscriptionID" HeaderText="Subscription ID" ReadOnly="True" Visible="false" />
+                  <asp:ButtonField ButtonType="Button" CommandName="Select" HeaderText="Delete" ShowHeader="True" Text="Delete" ControlStyle-CssClass="btn btn-outline-dark"  />        
+
+            <asp:ImageField DataImageUrlField="SubscriptionImage" HeaderText="Image" ControlStyle-Width="100" ControlStyle-Height = "100">
+            </asp:ImageField>
+            <asp:BoundField DataField="SubscriptionName" HeaderText="Subscription Name" SortExpression="SubscriptionName" />
+            <asp:BoundField DataField="SubscriptionPrice" HeaderText="Price" SortExpression="SubscriptionPrice" />
+            <asp:BoundField DataField="SubscriptionBillingTime" HeaderText="Billing Time" SortExpression="SubscriptionBillingTime" />
+            <asp:BoundField DataField="SubscriptionDescription" HeaderText="Description" SortExpression="SubscriptionDescription" />
+     
+            <asp:CommandField ButtonType="Button" HeaderText="Edit" ShowEditButton="True" ShowHeader="True" />
+        </Columns>
+                   
+    </asp:GridView>
+
+    <br />
+    <br />
+    <button type="button" class="btn btn-success float-right" data-toggle="modal" data-target="#myModal">Add New Subscription</button>
+
+<!-- Modal -->
+<div id="myModal" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+    
+        <h4 class="modal-title">Add New Subscription</h4>
+      </div>
+      <div class="modal-body">
+          <div class="form-group row justify-content-around">
+              <label for="txtNewSubscriptionName" class="col-sm-5 col-form-label"> Subscription Name</label>
+        <asp:TextBox ID="txtNewSubscriptionName" runat="server" CssClass="col-sm-5"> </asp:TextBox>
+        <br />
+                <label for="txtNewSubscriptionDescription" class="col-sm-5 col-form-label"> Subscription Description</label>
+        <asp:TextBox ID="txtNewSubscriptionDescription" runat="server" CssClass="col-sm-5"> </asp:TextBox>
+              <br />
+                <label for="txtNewSubcriptionPrice" class="col-sm-5 col-form-label"> Subscription Price</label>
+        <asp:TextBox ID="txtNewSubscriptionPrice" runat="server" CssClass="col-sm-5"> </asp:TextBox>
+              <br />
+                <label for="txtNewSubscriptionBilling" class="col-sm-5 col-form-label"> Subscription Billing Time</label>
+        <asp:TextBox ID="txtNewSubscriptionBilling" runat="server" CssClass="col-sm-5"> </asp:TextBox>
+
+              <br />
+              <label for="imageUpload" class="col-sm-5 col-form-label"> Subscription Image</label>
+              <asp:FileUpload ID="FileUpload1" runat="server" />
+
+          </div>
+       
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Close</button>
+              <asp:Button type="button" class="btn btn-primary" ID="btnAddNewSubscription" runat="server" Text="Add"></asp:Button>
+      </div>
+    </div>
+
+  </div>
+</div>
+
+    
+
+
+
 </asp:Content>
+
--- CIS3342-TermProject/AdminSubscription.aspx.cs
+++ CIS3342-TermProject/AdminSubscription.aspx.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using EcommerceLibrary;
+
 
 namespace CIS3342_TermProject
 {
@@ -12,6 +16,170 @@ namespace CIS3342_TermProject
         protected void Page_Load(object sender, EventArgs e)
         {
 
+            DBConnect objDB = new DBConnect();
+            SqlCommand objCommand = new SqlCommand();
+
+            objCommand.CommandType = CommandType.StoredProcedure;
+            objCommand.CommandText = "TP_GetAllSubscriptions";
+            DataSet myDS = objDB.GetDataSetUsingCmdObj(objCommand);
+
+            if (myDS.Tables[0].Rows.Count > 0)
+            {
+                gvSubscriptions.DataSource = myDS;
+                String[] names = new string[1];
+                names[0] = "SubscriptionID";
+                gvSubscriptions.DataKeyNames = names;
+                gvSubscriptions.DataBind();
+            }
+
         }
+
+
+
+        protected void gvSubscriptions_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void gvSubscriptions_RowEditing(Object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            gvSubscriptions.EditIndex = e.NewEditIndex;
+            gvSubscriptions.DataBind();
+
+        }
+
+        protected void gvSubscriptions_RowUpdating(Object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+        {
+
+
+
+            int rowIndex = e.RowIndex;
+            string selectedSubID = gvSubscriptions.DataKeys[rowIndex].Value.ToString();
+
+            TextBox TboxSubscriptionName;
+            TboxSubscriptionName = (TextBox)gvSubscriptions.Rows[rowIndex].Cells[3].Controls[0];
+
+
+            if (TboxSubscriptionName.Text == "")
+            {
+                lblError.Text = "Please enter a valid subscription name..";
+            }
+            else if (TboxSubscriptionName.Text != "")
+            {
+                lblError.Text = "";
+            }
+
+            TextBox TboxSubscriptionPrice;
+            TboxSubscriptionPrice = (TextBox)gvSubscriptions.Rows[rowIndex].Cells[4].Controls[0];
+
+            if(TboxSubscriptionPrice.Text == "")
+            {
+                lblError.Text = " Please enter a valid subscription price..";
+
+            }
+
+            else if(TboxSubscriptionPrice.Text!="")
+            {
+                lblError.Text = "";
+            }
+
+            TextBox TboxSubscriptionBillingTime;
+            TboxSubscriptionBillingTime = (TextBox)gvSubscriptions.Rows[rowIndex].Cells[5].Controls[0];
+
+            if(TboxSubscriptionBillingTime.Text=="")
+            {
+                lblError.Text = " Please enter a valid billing time";
+
+
+            }
+
+            else if(TboxSubscriptionBillingTime.Text!="")
+            {
+                lblError.Text = "";
+            }
+
+            TextBox TboxSubscriptionDescription;
+            TboxSubscriptionDescription = (TextBox)gvSubscriptions.Rows[rowIndex].Cells[6].Controls[0];
+
+            if(TboxSubscriptionDescription.Text=="")
+            {
+                lblError.Text = " Please enter a valid subscription description";
+
+
+            }
+
+            else if (TboxSubscriptionDescription.Text!="") {
+                lblError.Text = "";
+            }
+
+            if(lblError.Text=="")
+            {
+
+                string newName = TboxSubscriptionName.Text;
+                string newBilling = TboxSubscriptionBillingTime.Text;
+                string newPrice = TboxSubscriptionPrice.Text;
+                string newDescription = TboxSubscriptionDescription.Text;
+
+                DBConnect objDB = new DBConnect();
+                SqlCommand objCommand = new SqlCommand();
+
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_EditSubscription";
+                objCommand.Parameters.AddWithValue("@SubscriptionID", selectedSubID);
+                objCommand.Parameters.AddWithValue("@NewName", newName);
+                objCommand.Parameters.AddWithValue("@NewBilling", newBilling);
+                objCommand.Parameters.AddWithValue("@NewPrice", newPrice);
+                objCommand.Parameters.AddWithValue("@NewDescription", newDescription);
+
+                objDB.DoUpdateUsingCmdObj(objCommand);
+
+                gvSubscriptions.EditIndex = -1;
+                gvSubscriptions.DataBind();
+                Response.Redirect("AdminSubscription.aspx");
+                
+            }
+        }
+
+
+        protected void gvSubscriptions_RowCommand(Object sender, System.Web.UI.WebControls.GridViewCommandEventArgs e)
+        {
+            int rowIndex = int.Parse(e.CommandArgument.ToString());
+            string selectedSubID = gvSubscriptions.DataKeys[rowIndex].Value.ToString();
+
+            if (e.CommandName == "Select")
+            {
+                DBConnect objDB = new DBConnect();
+                SqlCommand objCommand = new SqlCommand();
+
+                objCommand.CommandType = CommandType.StoredProcedure;
+                objCommand.CommandText = "TP_DeleteSubscription";
+
+                objCommand.Parameters.AddWithValue("@SubscriptionID", selectedSubID);
+
+                objDB.DoUpdateUsingCmdObj(objCommand);
+                gvSubscriptions.DataBind();
+                Response.Redirect("AdminSubscription.aspx");
+
+            }
+        }
+
+
+
+
+
+
+
+
+        protected void gvSubscriptions_RowCancelingEdit(Object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            gvSubscriptions.EditIndex = -1;
+            gvSubscriptions.DataBind();
+
+        }
+
+     
+
     }
+
 }
+    
